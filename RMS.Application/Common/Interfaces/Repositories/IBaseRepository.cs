3c23453a70374193032547ea4d53fd259ba58932
Huseyn Haydarov <huseynhaydarov0@gmail.com>
2024-04-10 16:25:55 +0500

    Implement repositories

--- RMS.Application/Common/Interfaces/Repositories/IBaseRepository.cs
+++ RMS.Application/Common/Interfaces/Repositories/IBaseRepository.cs
@@ -9,5 +9,12 @@ namespace RMS.Application.Common.Interfaces.Repositories
 {
     public interface IBaseRepository<T> where T : EntityBase
     {
+        Task<T> GetById(int id);
+        Task<IEnumerable<T>> GetAll();
+        Task<T> Create(T entity);
+        Task<T> Update(T entity);
+        Task Delete(int id);
+
+
     }
 }
--- RMS.Infrastructure/Persistence/Repositories/BaseRepository.cs
+++ RMS.Infrastructure/Persistence/Repositories/BaseRepository.cs
@@ -7,6 +7,32 @@ using System.Threading.Tasks;
 
 namespace RMS.Infrastructure.Persistence.Repositories { }
 
-    //public class BaseRepository<TEntity> : IBaseRepository<TEntity>
+public class BaseRepository<TEntity> : IBaseRepository<TEntity>
+{
+    public Task<TEntity> Create(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<TEntity>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TEntity> GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TEntity> Update(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+}
 
 
