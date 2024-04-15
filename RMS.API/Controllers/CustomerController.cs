87dddf9b548b9b4ef77a77b82912509b55d8bfa5
Huseyn Haydarov <huseynhaydarov0@gmail.com>
2024-04-15 14:40:57 +0500

    Add CustomerController

---
+++ RMS.API/Controllers/CustomerController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RMS.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet]
+        public void Test()
+        {
+
+        }
+    }
+}
