527f27e92fc5ca9e58da9b903aa71a0a2710cf51
Huseyn Haydarov <huseynhaydarov0@gmail.com>
2024-04-16 09:13:33 +0500

    Add Requests & Responses

---
+++ RMS.Application/Requests/OrderItemsRequests/OrderItemRequestModel.cs
@@ -0,0 +1,17 @@
+﻿using RMS.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMS.Application.Requests.OrderItemsRequests
+{
+    public class OrderItemRequestModel : BaseRequest
+    {
+        public int Id { get; set; }
+        public int MenuId { get; set; }
+        public double Count { get; set; }   
+        public OrderStatus Status { get; set; }
+    }
+}
---
+++ RMS.Application/Responses/PaymentResponses/PaymentResponseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMS.Application.Responses.PaymentResponses
+{
+    public class PaymentResponseModel : BaseResponse
+    {
+        public int Id { get; set; }
+    }
+}
