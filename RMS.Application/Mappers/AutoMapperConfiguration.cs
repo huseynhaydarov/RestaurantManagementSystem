cc804b73b12b0cfc089d8f2acd019d08b3ea65e8
Huseyn Haydarov <huseynhaydarov0@gmail.com>
2024-04-17 13:22:09 +0500

    Latest changes

--- RMS.Application/Mappers/AutoMapperConfiguration.cs
+++ RMS.Application/Mappers/AutoMapperConfiguration.cs
@@ -1,18 +1,6 @@
 ﻿using AutoMapper;
 using RMS.Application.Requests.CustomerRequests;
-using RMS.Application.Requests.MenuItemRequestModel;
-using RMS.Application.Requests.OrderItemsRequests;
-using RMS.Application.Requests.OrderRequests;
-using RMS.Application.Requests.PaymentRequests;
-using RMS.Application.Requests.ReservationRequests;
-using RMS.Application.Requests.TableRequests;
 using RMS.Application.Responses.CustomerResponses;
-using RMS.Application.Responses.MenuItemResponses;
-using RMS.Application.Responses.OrderItemResponses;
-using RMS.Application.Responses.OrderResponses;
-using RMS.Application.Responses.PaymentResponses;
-using RMS.Application.Responses.ReservationResponses;
-using RMS.Application.Responses.TableResponses;
 using RMS.Domain.Entities;
 using System;
 using System.Collections.Generic;
@@ -22,12 +10,12 @@ using System.Threading.Tasks;
 
 namespace RMS.Application.Mappers
 {
-    public class AutoMapperConfiguration : Profile
+    internal class AutoMapperConfiguration : Profile
     {
         public AutoMapperConfiguration() 
         {
             CreateMap<CreateCustomerRequestModel, Customer>();
-            CreateMap<Customer, CustomerResponseModel>();
+            CreateMap<Customer, CustomerResponseModel> ();
             CreateMap<UpdateCustomerRequestModel, Customer>();
 
             CreateMap<CreateMenuItemRequestModel, MenuItem>();
